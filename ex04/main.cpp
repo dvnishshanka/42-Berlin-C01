9149e9513b6fda0b3e92f73964104b6e75c768c4
dvnishsha <dvnishshanka@gmail.com>
2024-09-06 16:48:46 +0200

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <fstream>
 
 #define INPUT_ERROR "Please enter valid parameters."
 #define INVALID_FILENAME "Please enter valid filename."
 
 int	main(int argc, char **argv)
 {
-	std::string	filename;
-	std::string	s1;
-	std::string	s2;
-
 	if (argc == 4)
 	{
-		filename = argv[1];
-		s1 = argv[2];
-		s2 = argv[3];
-		if (filename.empty())
+		std::string filename = argv[1];
+		std::string s1 = argv[2];
+		std::string s2 = argv[3];
+		std::string newFilename = filename + ".replace";
+
+		std::ifstream readFile(filename, std::ifstream::in);
+		if (!readFile.is_open())
 		{
-			std::cout << INVALID_FILENAME << std::endl;
+			std::cerr << INVALID_FILENAME << std::endl;
 			return (1);
 		}
-		std::cout << "OK" << filename <<" " << s1 << " "<< s2 << std::endl;
+
+		char c = readFile.get();
+		std::ofstream writeFile(newFilename);
+		while (readFile.good()) {
+			writeFile << c;
+			c = readFile.get();
+		}
+
+		readFile.close();
+		writeFile.close();
 	}
 	else
-		std::cout << INPUT_ERROR << std::endl;
+		std::cerr << INPUT_ERROR << std::endl;
 
 	return (0);
 }
